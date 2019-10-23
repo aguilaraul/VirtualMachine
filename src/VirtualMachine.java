a4b04dcce34482eac9fb79767f55e749d6c75e4a
Raul Aguilar <raula@live.com>
2019-10-22 17:37:01 -0700

    added VirtualMachine.java
    added a driver program for the virtual machine

---
+++ src/VirtualMachine.java
@@ -0,0 +1,44 @@
+﻿/*
+	@author	Raul Aguilar
+	@date	October 22, 2019
+*/
+import java.util.Scanner;
+
+public class VirtualMachine {
+	public static void main(String[] args) {
+		Scanner = keyboard = new Scanner(System.in);
+		Parser parser = new Parser();
+		CodeWriter codeWriter = new CodeWriter();
+		String inputFileName, outputFileName;
+		
+
+		// Open file from command line or console
+		if(args.length == 1) {
+			System.out.println("command line arg = " + args[0]);
+			inputFileName = args[0];
+		} else {
+			System.out.println("Please enter assembly file name you would like to assemble.);
+			System.out.println("Don't forget the .vm extension: ");
+			inputFileName = keyboard.nextLine();
+			keyboard.close();
+		}
+
+		outputFileName = inputFileName.substring(0, inputFileName.lastIndexOf('.')) + ".asm";
+		codeWriter.CodeWriter(outputFileName);
+
+		// Driver
+		parser.Parser(inputFileName);
+		while(parser.hasMoreCommands()) {
+			parser.advance();
+
+			// write to file
+			if(parser.getCommandType() = Command.C_ARITHMETIC) {
+				codeWriter.writeArithmetic(parser.arg1());
+			} else if (parser.getCommandType() == Command.C_PUSH || parser.getCommandType() == Command.C_POP) {
+				codeWriter.writePushPop(parser.getCommandType(), parser.arg1(), parser.arg2());
+		}
+
+		codeWriter.close();
+		System.out.println("Finished assemblng. Program exiting.");
+	}
+}
